4d324172cb3064e8598faf9e976de32b9e8632d2
Harold Treen <haroldtreen@gmail.com>
2016-07-25 20:56:16 -0500

    Task for testing libraries

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,16 @@ const options = { reporter: 'spec', harmony: true, grep: args.regex || '.' };
 
 gulp.task('test', () => {
     process.env.NODE_ENV = 'test';
-    return gulp.src('./tests/**/*-test.js', { read: false })
+    return gulp.src('./tests/*-test.js', { read: false })
 			.pipe(mocha(options));
+});
+
+gulp.task('test-libraries', () => {
+    process.env.NODE_ENV = 'test';
+    return gulp.src('./tests/library-tests/*-test.js', { read: false })
+			.pipe(mocha(options));
+});
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise ', p, ' reason: ', reason);
 });
